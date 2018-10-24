22a6a4d4e201b34edc8ecb2658012214ab751df8
nim94 <nikomoro1994@gmail.com>
2018-10-24 17:23:24 +0200

    fixed navbar transparent

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-//import 'owl.carousel2/dist/assets/owl.carousel.css';
-//import $ from 'jquery';
-//import 'imports?jQuery=jquery!owl.carousel';
-
-
 import 'bootstrap'
 import './sass/main.scss'
 import './js/owl-carousel.js'
@@ -10,7 +5,11 @@ import './js/navbar.js'
 
 
 $(document).ready(function(){
-    $('.contact, .contact-nav-wrapper').on('click', 'tap', function(){ $(this).css('background', '#c4db0d') });
+    
+    $('.contact, .nav-contact').on('click tap', function(){ 
+        $(this).css('background', '#c4db0d');
+    });  
+     
 });
 
 
