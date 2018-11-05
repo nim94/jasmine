06f7ad0caca00d0bed56b180ca09d71ded307e4a
nim94 <nikomoro1994@gmail.com>
2018-11-05 19:52:46 +0100

    overview background

--- src/index.js
+++ src/index.js
@@ -28,9 +28,31 @@ $(document).ready(function(){
         $('body, html').animate({
           scrollTop: 0,
         }, 300, 'linear');
-      });
+    });
+    $(window).scroll(function() {
+        if (isScrolledIntoView( $('#chi-sono') ) === true) {
+            $('#chi-sono').addClass('animated fadeInLeft');
+        }
+    });
+    $(window).scroll(function() {
+        if (isScrolledIntoView( $('#servizi') ) === true) {
+            $('#servizi').addClass('animated fadeInRight');
+        }
+    });
+    $(window).onload(function(){
+        $('.overview-logo').addClass('animated fadeInRight');
+    });    
 });
 
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
 
 
 
