1285044c243396f1c9781d065a39bf8fc2ae9e7f
nim94 <nikomoro1994@gmail.com>
2018-11-09 19:04:57 +0100

    slider servizi

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import 'bootstrap'
 import './sass/main.scss'
 import './js/navbar.js'
 import { relative } from 'path'
+
+import owlCarousel from 'owl.carousel2';
  
 
 
@@ -26,22 +28,39 @@ $(document).ready(function(){
     });
     $('.scrolltop-button').click(function(){
         $('body, html').animate({
-          scrollTop: 0,
+          scrollTop: $('#content-body').offset().top,
         }, 300, 'linear');
     });
     $(window).scroll(function() {
         if (isScrolledIntoView( $('#chi-sono') ) === true) {
             $('#chi-sono').addClass('animated fadeInLeft');
         }
-    });
-    $(window).scroll(function() {
         if (isScrolledIntoView( $('#servizi') ) === true) {
-            $('#servizi').addClass('animated fadeInRight');
+            $('#servizi h2').addClass('animated fadeInRight');
+            $('.item').each(function(){
+                $(this).addClass('animated fadeInRight');
+            });
+        }
+        if (isScrolledIntoView( $('#dieta-per-lo-sport') ) === true) {
+            $('#dieta-per-lo-sport').addClass('animated fadeInLeft');
         }
     });
-    $(window).onload(function(){
-        $('.overview-logo').addClass('animated fadeInRight');
-    });    
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    }); 
 });
 
 function isScrolledIntoView(elem) {
